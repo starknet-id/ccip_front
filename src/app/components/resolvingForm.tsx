825309d8542f20131b7f219afcf294e72e0d2cdb
Iris <iris.devillars@gmail.com>
2024-04-01 16:21:10 +0200

    fix: update to work with new starknetid.js function

--- src/app/components/resolvingForm.tsx
+++ src/app/components/resolvingForm.tsx
@@ -4,16 +4,8 @@ import { TextField } from "@mui/material";
 import { useMemo, useState } from "react";
 import btnStyles from "../../../styles/button.module.css";
 import { useIsValidDomain } from "../hooks/useIsValidDomain";
-import { Abi, Contract, Provider, shortString } from "starknet";
-import naming_abi from "../../../abi/naming.json";
+import { CallData, Provider, shortString } from "starknet";
 import { utils } from "starknetid.js";
-import { decimalToHex, extractArrayFromErrorMessage } from "../utils/utils";
-
-type CallResponse = {
-  address?: any;
-  res?: any;
-  error?: any;
-};
 
 export const ResolvingForm = () => {
   const [domain, setDomain] = useState("");
@@ -21,22 +13,13 @@ export const ResolvingForm = () => {
   const [error, setError] = useState<string | null>(null);
   const isDomainValid = useIsValidDomain(domain);
 
-  // We use Sequencer for now as the RPC provider is not returning detailed error messages
   const provider = useMemo(() => {
     return new Provider({
       rpc: {
-        nodeUrl: "https://rpc.starknet-testnet.lava.build",
+        nodeUrl: process.env.NEXT_PUBLIC_RPC_URL as string,
       },
     });
   }, []);
-
-  const contract = useMemo(() => {
-    return new Contract(
-      naming_abi.abi as Abi,
-      process.env.NEXT_PUBLIC_NAMING_CONTRACT as string,
-      provider
-    );
-  }, [provider]);
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setDomain(event.target.value);
@@ -47,64 +30,87 @@ export const ResolvingForm = () => {
   const submit = async () => {
     setError(null);
     if (!isDomainValid) return;
-    const encoded = domain
-      ? utils.encodeDomain(domain).map((elem) => elem.toString())
-      : [];
+    const response = await getAddressFromStarkName(domain);
+    setAddress(response);
+  };
 
-    const callResponse = await callContract(contract, encoded, []);
+  // todo: call starknetid.js function instead of this function
+  const getAddressFromStarkName = async (domain: string): Promise<string> => {
+    const contract = process.env.NEXT_PUBLIC_NAMING_CONTRACT as string;
+    const encodedDomain = utils
+      .encodeDomain(domain)
+      .map((elem) => elem.toString(10));
 
-    if (callResponse.res) {
-      const failureReason = callResponse.res[0];
-      if (failureReason === "offchain_resolving") {
-        const serverRes = await queryServer(
-          callResponse.res[1] + callResponse.res[2],
-          domain
-        );
-        if (serverRes.data) {
-          const callResponse2 = await callContract(contract, encoded, [
+    try {
+      return await tryResolveDomainSID(contract, encodedDomain, []);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log("error", error);
+        // extract server uri from error message
+        const data = extractArrayFromErrorMessageSID(String(error));
+        if (!data?.includes("offchain_resolving")) {
+          // if the error is not related to offchain resolving
+          throw new Error("Could not get address from stark name");
+        }
+        const uri = data.slice(1).join("");
+
+        // Query server
+        try {
+          const serverRes = await queryServerSID(uri, domain);
+          if (serverRes.error) {
+            throw new Error("Could not resolve domain");
+          }
+          // try resolving with hint
+          const hint: any[] = [
             serverRes.data.address,
             serverRes.data.r,
             serverRes.data.s,
             serverRes.data.max_validity,
-          ]);
-          if (callResponse2.address)
-            setAddress(decimalToHex(callResponse2.address as string));
-          else if (callResponse2.error) setError(String(callResponse2.error));
-          else setError("Error while resolving domain");
-        } else if (serverRes.error) {
-          console.log(serverRes);
-          setError(String(serverRes.error));
-        } else {
-          setError("Error while querying server");
+          ];
+          return await tryResolveDomainSID(contract, encodedDomain, hint);
+        } catch (error: any) {
+          throw new Error(`Could not resolve domain : ${error.message}`);
         }
       } else {
-        console.log("err from call");
+        throw new Error("Could not get address from stark name");
       }
     }
   };
 
-  const callContract = async (
-    contract: Contract,
-    encoded: string[],
-    hint: string[]
-  ): Promise<CallResponse> => {
-    try {
-      const res = await contract.call("resolve", [
-        encoded,
-        shortString.encodeShortString("starknet"),
-        hint,
-      ]);
-      return { address: res };
-    } catch (err) {
-      const data = extractArrayFromErrorMessage(String(err));
-      if (data) return { res: data };
-      else return { error: err };
-    }
+  const tryResolveDomainSID = async (
+    contract: string,
+    encodedDomain: string[],
+    hint: any = []
+  ): Promise<string> => {
+    const addressData = await provider.callContract({
+      contractAddress: contract,
+      entrypoint: "domain_to_address",
+      calldata: CallData.compile({ domain: encodedDomain, hint }),
+    });
+    return addressData.result[0];
   };
 
-  const queryServer = async (serverUri: string, domain: string) => {
+  const extractArrayFromErrorMessageSID = (errorMsg: string) => {
+    const pattern = /Execution failed\. Failure reason: \((.*?)\)\./;
+    const match = errorMsg.match(pattern);
+
+    if (match && match[1]) {
+      const values = match[1].split(",").map((value) => value.trim());
+      const res = values.map((entry) => {
+        const hexMatch = entry.match(/(0x[0-9a-f]+)/i);
+        if (hexMatch && hexMatch[1]) {
+          return shortString.decodeShortString(hexMatch[1]);
+        }
+      });
+      return res;
+    }
+
+    return null;
+  };
+
+  const queryServerSID = async (serverUri: string, domain: string) => {
     try {
-      const response = await fetch(`${serverUri}/resolve?domain=${domain}`);
+      const response = await fetch(`${serverUri}${domain}`);
 
       if (!response.ok) {
         const errorResponse = await response.text();
